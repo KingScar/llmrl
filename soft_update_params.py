a31c5ac71b03a8de145988bc01a88abd5e7e65bc
KingScar <59480062+KingScar@users.noreply.github.com>
2024-12-01 18:09:04 +0800

    Add files via upload

---
+++ soft_update_params.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def soft_update_params(net, target_net, tau):
+    for param, target_param in zip(net.parameters(), target_net.parameters()):
+        target_param.data.copy_(
+            tau * param.data + (1 - tau) * target_param.data
+        )
+
+
